7c7a85b0f0c29e5a50e25b38d67844e31a24ade1
prastowoadi18 <prastowoadiwork@gmail.com>
2023-05-20 18:17:59 +0700

    add cdd

--- app/components/OurMoment/index.tsx
+++ app/components/OurMoment/index.tsx
@@ -4,6 +4,11 @@ import { Mr_De_Haviland, Gabriela } from "next/font/google";
 
 import LightGallery from "lightgallery/react";
 
+// import styles
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+
 // import plugins if you need
 import lgThumbnail from "lightgallery/plugins/thumbnail";
 import lgZoom from "lightgallery/plugins/zoom";
